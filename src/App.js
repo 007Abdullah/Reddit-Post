4e892e096e822e607844b68233240ab6ac939e87
Muhammad Abdullah <kb337137@gmail.com>
2021-02-16 04:57:48 +0500

    added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { Container, TextField, TextareaAutosize, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import { useState } from 'react'
 
 function App() {
+  const [posts, setposts] = useState({});
+  function handelSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
+
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }));
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container maxWidth="sm">
+        <div className={classes.root}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <Paper className={classes.paper}>
+                <form className={classes.root} action="" onSubmit={handelSubmit}>
+                  <TextField id="Title" label="Title" value={posts.name} onChange={(e) => { setposts(e.target.value) }} color="secondary" />
+                  <br />
+                  <br />
+                  <TextareaAutosize aria-label="empty textarea" placeholder="Write Text" id="textarea" value={posts.text} onChange={(e) => { setposts(e.target.value) }} />
+                  <br />
+                  <br />
+                  {/* <TextareaAutosize aria-label="minimum height" rowsMin={5} colMin={10} placeholder="Write Text" /> */}
+                  <Button variant="contained">POST</Button>
+                </form>
+              </Paper>
+            </Grid>
+          </Grid>
+        </div>
+      </Container>
+    </>
   );
 }
 
